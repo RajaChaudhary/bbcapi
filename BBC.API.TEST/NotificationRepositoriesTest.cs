5d14ccb2afe4b4c15c98124429c4d7bc38a42dbd
User <User@LP-R90GXVZ1>
2019-03-02 15:13:03 +0530

    Added Test Project

---
+++ BBC.API.TEST/NotificationRepositoriesTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class NotificationRepositoriesTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
